a28a3664ca6c8f57deafa219df90861d8e3b91f0
ayush-27112 <109783951+ayush-27112@users.noreply.github.com>
2024-02-13 16:27:33 +0000

    #

---
+++ VarConstLet.js
@@ -0,0 +1,14 @@
+let first='ayush.27112@gmail.com'
+const second = 1233
+var third = "1234"
+fourth='jaipur'
+// var has a problem on scope read on freecodecamp.com
+first="ayush.s.singh@slrtce.in"
+// second=3443
+third='9990'
+fourth='Mumbai'
+let fifth
+
+console.log(third);
+
+console.table([first, second, third, fourth, fifth])
---
+++ datatypes.js
@@ -0,0 +1,4 @@
+'use strict'; //treat all JS code as newer version
+//today all works by default in strict mode
+
+//alert("hello") we are using node js and not browser

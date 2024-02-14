10d5229254b818ac40889712c19b360f82b7f571
ayush-27112 <109783951+ayush-27112@users.noreply.github.com>
2024-02-14 14:48:16 +0000

    #

---
+++ datatypes2.js
@@ -0,0 +1,14 @@
+//primitive datatypes are string, number, null, undefined, boolean, Symbol, BigInt
+
+//non primitive datatypes are array, objects, functions
+const arr=['Anika','Akansha', 'Atharva', 'Ananya']
+console.log(typeof(arr));
+
+let myObj={name:"Ayush",age:"21"} //dictnary like in python
+console.log(typeof(myObj));
+
+const myFun =function(){
+    console.log("I am Function")
+}
+
+console.log(typeof myFun);
